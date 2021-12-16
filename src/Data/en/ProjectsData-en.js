734b6f5e12b8ec3163c561e0447dc2e0f1173987
Mario Quirós Luna <marioquiros21@yahoo.com>
2021-12-16 14:58:35 -0600

    Update Data

--- src/Data/en/ProjectsData-en.js
+++ src/Data/en/ProjectsData-en.js
@@ -14,12 +14,16 @@ const ProjectsData = [
 			{
 				tag: 'Git',
 				icon: 'https://res.cloudinary.com/dusx4zdpz/image/upload/v1639372535/portfolio/git_l7ljml.png'
+			},
+			{
+				tag: 'MarkDown',
+				icon: 'https://res.cloudinary.com/dusx4zdpz/image/upload/v1639688243/portfolio/md_rfwrlm.png'
 			}
 		],
 		description: 'This project is my personal website. That works as a place to show my work and provide a place where people can get to know how I work better.',
 		specifications: 'I really liked this project. Since I learned a lot about React while developing it, looking to implement best practices, clean and orderly code. In this project use react-router for navigation, also practice the use of Hooks and Context, to the styles use styled-component in which it use global styles which are repeat in different places and also use ThemeProvider to handle the theme of my application.',
 		repository: 'https://github.com/MarioQuirosLuna/MarioQL.dev',
-		liveUrl: ''
+		liveUrl: 'https://marioql-dev.vercel.app/'
 	},
 	{
 		nameProject: 'Frontend-SewingShop',
