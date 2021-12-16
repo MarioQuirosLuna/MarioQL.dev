cf20640531e37618f6bf3ee0e297eec72dd0b548
Mario Quirós Luna <marioquiros21@yahoo.com>
2021-12-16 12:05:50 -0600

    Google Analytics

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import { useNavigate, useLocation } from 'react-router-dom'
+import ReactGA from 'react-ga'
 
 import MenuContext from '../../Context/MenuContext'
 import TranslationContext from '../../Context/TranslationContext'
@@ -29,6 +30,10 @@ const Header = ({ goBack, translate }) => {
 	const { pathname } = useLocation()
 
 	const handleClick = () => {
+		ReactGA.event({
+			category: 'Button',
+			action: 'Click the button menu'
+		})
 		setMenu(!menu)
 		document.getElementById('body').classList.toggle('overflow--hidden')
 	}
