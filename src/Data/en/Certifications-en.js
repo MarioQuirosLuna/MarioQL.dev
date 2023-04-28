154fee73a99377edf2ffd854037d80e7d44ade8e
Mario Quirós Luna <marioquiros21@yahoo.com>
2023-04-28 09:47:54 -0600

    Update Certifications-en.js

--- src/Data/en/Certifications-en.js
+++ src/Data/en/Certifications-en.js
@@ -1,7 +1,77 @@
 const Certifications = [
 	{
+		name: 'Learning NodeJS (2023)',
+		url: 'https://www.linkedin.com/learning/certificates/43b4733dd555a79fb0d6ae21617386106a6104889fb50f4dbce151604824ac11',
+		company: 'LinkedIn'
+	},
+	{
+		name: 'JavaScript Avanzado 2 (2023)',
+		url: 'https://www.linkedin.com/learning/certificates/281ff5851b6b2acb5a543d30f6486ac40c1f79183f301b4c68c6dd5123b4ead1',
+		company: 'LinkedIn'
+	},
+	{
+		name: 'JavaScript Avanzado 1 (2023)',
+		url: 'https://www.linkedin.com/learning/certificates/ad886dfa3df048956136ed5d9bc5293241fe979b7df235f0ac80d659830bb8da',
+		company: 'LinkedIn'
+	},
+	{
+		name: 'Programming Foundations: Software Testing/QA (2023)',
+		url: 'https://www.linkedin.com/learning/certificates/3df063af6571230df5351e4a312adddbf0d99790f3e3ff2ada398bece5392cf8',
+		company: 'LinkedIn'
+	},
+	{
+		name: 'Angular avanzado: Trabajo con API (2023)',
+		url: 'https://www.linkedin.com/learning/certificates/4bfc9517e7f2d825ba25b848a743d6fb8299a6cbfa8f92f5126eb12e39799b52',
+		company: 'LinkedIn'
+	},
+	{
+		name: 'Angular Esencial (2023)',
+		url: 'https://www.linkedin.com/learning/certificates/6e866d93fdceaf6322aac5228f3f65a843dcc1c4b541ee6e1e595e24852b1449',
+		company: 'LinkedIn'
+	},
+	{
+		name: 'Diseño de interfaz (UI) avanzado (2023)',
+		url: 'https://www.linkedin.com/learning/certificates/c2f36fc779843b38c7fea5b6cf596c7b0a4ff86a5855ff4d3c042f07cb3cdb5e',
+		company: 'LinkedIn'
+	},
+	{
+		name: 'Diseño de interfaz (UI) esencial (2023)',
+		url: 'https://www.linkedin.com/learning/certificates/9800b3cbd63c4b331cb4f30ca512cb80c1733d5cad2dbc9f9e0a86a92c4e2f80',
+		company: 'LinkedIn'
+	},
+	{
+		name: 'Experiencia de usuario (UX) avanzado (2023)',
+		url: 'https://www.linkedin.com/learning/certificates/c561ab5f17a55f30ad9faed81c267c36dd258f5930835417e45fefb5d7c6f0be',
+		company: 'LinkedIn'
+	},
+	{
+		name: 'Experiencia de usuario (UX) esencial (2023)',
+		url: 'https://www.linkedin.com/learning/certificates/487353e56afdb160d5dc7fe89915adc618477f5b78e69c34ef9fa95bb7b19429',
+		company: 'LinkedIn'
+	},
+	{
+		name: 'Android Development Essential Training (2023)',
+		url: 'https://www.linkedin.com/learning/certificates/d04c60039a5d7f6e1571ec672a64d4b3b57366c6b7563ad0824a409805c92ec9',
+		company: 'LinkedIn'
+	},
+	{
+		name: 'Learning React Native (2023)',
+		url: 'https://www.linkedin.com/learning/certificates/f22e6a17f0ec7da94fc3dba56c708e5051ab559289a1b8e092576a980e22363c',
+		company: 'LinkedIn'
+	},
+	{
+		name: ' React Native Essential Training (2023)',
+		url: 'https://www.linkedin.com/learning/certificates/257e170cd43075f93b99274f75c6a1cfec0c925a429f6160dcbfabf1f1210b5a',
+		company: 'LinkedIn'
+	},
+	{
+		name: 'Scrum Fundamentals Certified(SFC) (2022)',
+		url: 'https://www.scrumstudy.com/certification/verify?type=SFC&number=953368',
+		company: 'SCRUMstudy'
+	},
+	{
 		name: 'Docker Essencial',
-		url: 'https://www.udemy.com/certificate/UC-6b107b2e-f350-4afd-9359-55a4ab0efb2a/',
+		url: 'https://www.linkedin.com/learning/certificates/b4335aee084c1f0911b423e40714c6149e147ad2e6deb7161b8360a53a06df0d',
 		company: 'LinkedIn'
 	},
 	{
