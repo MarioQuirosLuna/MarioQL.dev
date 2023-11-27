d3bb4bb5a71f5ce0133f61e5383028cbd132f9d2
MarioQuirosL <marioql.dev@gmail.com>
2023-11-27 16:28:58 -0600

    Updated Stack

--- src/Data/en/Stack-en.js
+++ src/Data/en/Stack-en.js
@@ -31,32 +31,24 @@ const Stack = {
 	],
 	technologies: [
 		{
-			tag: 'HTML',
-			icon: 'https://res.cloudinary.com/dusx4zdpz/image/upload/v1638987538/portfolio/html-5_d9vbhr.png'
-		},
-		{
-			tag: 'CSS',
-			icon: 'https://res.cloudinary.com/dusx4zdpz/image/upload/v1638987538/portfolio/css-3_msdndh.png'
-		},
-		{
-			tag: 'Javascript',
-			icon: 'https://res.cloudinary.com/dusx4zdpz/image/upload/v1638987538/portfolio/js_l2gveb.png'
-		},
-		{
-			tag: 'Angular',
-			icon: 'https://res.cloudinary.com/dusx4zdpz/image/upload/v1679101252/portfolio/angular_qmv8sr.png'
-		},
-		{
-			tag: 'React',
-			icon: 'https://res.cloudinary.com/dusx4zdpz/image/upload/v1638987539/portfolio/react_lriiap.png'
+			tag: 'Flutter',
+			icon: 'https://res.cloudinary.com/dusx4zdpz/image/upload/v1699389705/portfolio/flutter_awlreq.png'
 		},
 		{
 			tag: 'React Native',
 			icon: 'https://res.cloudinary.com/dusx4zdpz/image/upload/v1638987539/portfolio/react_lriiap.png'
 		},
 		{
-			tag: 'Styled-components',
-			icon: 'https://res.cloudinary.com/dusx4zdpz/image/upload/v1638987539/portfolio/styledComponets_laq7fu.png'
+			tag: 'Javascript',
+			icon: 'https://res.cloudinary.com/dusx4zdpz/image/upload/v1638987538/portfolio/js_l2gveb.png'
+		},
+		{
+			tag: 'React',
+			icon: 'https://res.cloudinary.com/dusx4zdpz/image/upload/v1638987539/portfolio/react_lriiap.png'
+		},
+		{
+			tag: 'Angular',
+			icon: 'https://res.cloudinary.com/dusx4zdpz/image/upload/v1679101252/portfolio/angular_qmv8sr.png'
 		},
 		{
 			tag: 'Bootstrap5',
@@ -71,8 +63,12 @@ const Stack = {
 			icon: 'https://res.cloudinary.com/dusx4zdpz/image/upload/v1638987539/portfolio/node_k24mps.png'
 		},
 		{
-			tag: 'Express',
-			icon: 'https://res.cloudinary.com/dusx4zdpz/image/upload/v1639086560/portfolio/expressjs_gqenna.png'
+			tag: 'HTML',
+			icon: 'https://res.cloudinary.com/dusx4zdpz/image/upload/v1638987538/portfolio/html-5_d9vbhr.png'
+		},
+		{
+			tag: 'CSS',
+			icon: 'https://res.cloudinary.com/dusx4zdpz/image/upload/v1638987538/portfolio/css-3_msdndh.png'
 		},
 		{
 			tag: 'Git',
@@ -83,15 +79,11 @@ const Stack = {
 			icon: 'https://res.cloudinary.com/dusx4zdpz/image/upload/v1638987538/portfolio/java_fuexzh.png'
 		},
 		{
-			tag: 'PHP',
-			icon: 'https://res.cloudinary.com/dusx4zdpz/image/upload/v1638987539/portfolio/php_bvlguk.png'
+			tag: 'Kotlin',
+			icon: 'https://res.cloudinary.com/dusx4zdpz/image/upload/v1701124025/Kotlin_fwjxoc.png'
 		},
 		{
-			tag: 'C#',
-			icon: 'https://res.cloudinary.com/dusx4zdpz/image/upload/v1638987537/portfolio/c-sharp_rjqpqh.png'
-		},
-		{
-			tag: '.NetCore',
+			tag: '.Net',
 			icon: 'https://res.cloudinary.com/dusx4zdpz/image/upload/v1638987539/portfolio/NET_Core_jfsgq5.png'
 		},
 		{
